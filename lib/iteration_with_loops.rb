cb7630bcfe8ca9842d1cceb8924033bafddf7ab5
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-02 03:28:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_min_in_nested_arrays(src)
   while count < src.length do
     index = 0
     while index < src[count].length do
-      if src[count][index] < src[count].each 
+      if src[count][index] < src[count].each_char { |min|  }
         smallest << src[count][index]
         index +=1
       end
